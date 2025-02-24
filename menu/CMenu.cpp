3f6e4e7e2ac034db47863c9b95dd601d81075930
EVgeny26 <usenko.euvgeny@mail.ru>
2025-02-24 12:44:23 +0300

    second comit

---
+++ menu/CMenu.cpp
@@ -0,0 +1,42 @@
+#include "./CMenu.h"
+
+CMenu::CMenu(string title, CMenuItem *items, size_t count) : title(title), items(items), count(count) {}
+
+int CMenu::getSelect() const {
+    return select;
+}
+
+bool CMenu::isRun() const {
+    return running;
+}
+
+size_t CMenu::getCount() const {
+    return count;
+}
+
+string CMenu::getTitle() {
+    return title;
+}
+
+CMenuItem *CMenu::getItems() {
+    return items;
+}
+
+void CMenu::print() {
+    for (size_t i{}; i < count; ++i) {
+        std::cout << i + 1 << ". ";
+        items[i].print();
+        std::cout << std::endl;
+    }
+    std::cout << "0. выйти" << std::endl;
+}
+
+int CMenu::runCommand() {
+    do{
+        print();
+        std::cout << "\n   Ввод >> ";
+        std::cin >> select;
+    }while(select<0 || select>count);
+    if (select==0) return 0;
+    return items[select - 1].run();
+}
